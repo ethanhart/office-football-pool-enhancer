00922734d7555771b4e00e4cdc09152ffed25c2b
Ethan Hart <ethan.john.hart@gmail.com>
2019-08-06 14:07:45 -0700

    Fixed for new table layout, sorting fix, visual improvements

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This Chrome extension adds some addtional features and stats to the Office Footb
  
 ### Updates
 * 09-22-2015: Available in Chrome Web Store [here](https://chrome.google.com/webstore/detail/office-football-pool-enha/giddgbpfifkedgnlonpcpbhnhbpkdapo?hl=en-US&gl=US)
+* 08-06-2019: New webpage layout required some minor fixes. Also added additional sorting functionality and a few minor visual adjustments/enhancements.
 
 
 #### Manual Installation:
--- js/ofps.js
+++ js/ofps.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Office Football Pool Enhanced Standings
-// @version 0.1
+// @version 2.0
 // @namespace http://github.com/ethanhart
 // @description Add some additional features to the Office Football Pool standings page
 // @author Ethan Hart (ethan.john.hart@gmail.com)
@@ -19,26 +19,59 @@ function getIntValue(value) {
     return int_value;
 }
 
-function addAverageRow(mean) {
-    var tr = document.getElementById('standingsTable').tHead.children[0], th = document.createElement('th');
-    th.innerHTML = "Delta";
-    tr.insertBefore(th, tr.children[3]);
-    
+function addAverageRow(mean, meanRisked) {
+    var tr = document.getElementsByTagName('thead')[0].children[0]; //Get the table header (only works if it's the first header in the page)
+
+    // Create the Delta header element and add some functionality/formatting
+    var th = document.createElement('th');  //Create the element for the new header entry
+    th.style.textAlign = "Right";
+    var delta_a = document.createElement('a') //Create attribute sub-element
+    delta_a.innerHTML = "Delta";
+    delta_a.href = "javascript:void(3);";
+
+    th.appendChild(delta_a)  // Add the a element
+    tr.insertBefore(th, tr.children[3]);  // Insert the new header element into the header
+
+    // Modify the onclick events to properly reflect new header sequence
+    // NOTE: Sorting seems to work ok for most fields except for 4 ("Risked"). Cannot reverse sorting.
+    tr.children[3].children[0].setAttribute('onclick', "sortTable('bstandings', 3, true, true, false, false, 'numeric')");
+    tr.children[4].children[0].href = "javascript:void(4);";
+    tr.children[4].children[0].setAttribute('onclick', "sortTable('bstandings', 4, true, true, true, false, 'numeric')");
+
+    // UNSED SECTION SAVED FOR POSTERITY
+    //delta_a.setAttribute("href": "javascript:void(3);");
+    //delta_a.onclick = "sortTable('bstandings',3, true, true);";
+    //delta_a.onclick = "sortTable('bstandings',3, true, true)";
+    //th.onclick = "sortTable('bstandings',3, true, true, false, false, 'numeric',);";
+    //delta_a.addEvent("onclick": "sortTable('bstandings',3, true, true, false, false, 'numeric')");
+    //th.onclick="sortTable('bstandings',3, true, true, false, false, 'numeric',);";
+    //th.children[0].setAttribute('onclick', "sortTable('bstandings', 3, true, true)");
+    //th.children[0].setAttribute('onclick', "sortTable('bstandings', 3, true, false, false, false, 'numeric')");
+    //tr.children[4].children[0].setAttribute('onclick', "sortTable('bstandings', 4)");
+
     for (var i = 0, row; row = table.rows[i]; i++) {
         var new_picks_cell = row.insertCell(4);
         var orig_picks = row.cells[3].innerHTML;
         new_picks_cell.innerHTML = orig_picks;
         var cell_user_delta = row.cells[3]
         var cell_user_shares = row.cells[2]
+        var cell_user_risked = row.cells[4]
         var value = getIntValue(cell_user_shares.innerHTML);
         cell_user_delta.innerHTML = value - mean;
-        cell_user_delta.innerHTML = value - mean;
+        cell_user_risked.style.textAlign ="Right";
         if (value < mean) {
-            cell_user_shares.style.backgroundColor = "red";
-            cell_user_delta.style.backgroundColor = "red";
+            cell_user_shares.style.backgroundColor = "crimson";
+            cell_user_delta.style.backgroundColor = "indianred";
         } else {
-            cell_user_shares.style.backgroundColor = "green";
-            cell_user_delta.style.backgroundColor = "green";
+            cell_user_shares.style.backgroundColor = "forestgreen";
+            cell_user_delta.style.backgroundColor = "mediumseagreen";
+        }
+        
+        var risked_value = getIntValue(cell_user_risked.innerHTML);
+        if (risked_value < meanRisked) {
+            cell_user_risked.style.backgroundColor = "lightcoral";
+        } else {
+            cell_user_risked.style.backgroundColor = "lightgreen";
         }
     }
 
@@ -49,18 +82,30 @@ function addAverageRow(mean) {
     var cell_shares = row.insertCell(2);
     var cell_delta = row.insertCell(3);
     var cell_picks = row.insertCell(4);
-    cell_shares.innerHTML = mean;
+    cell_shares.innerHTML = numberWithCommas(mean);
     cell_shares.style.backgroundColor = "yellow";
     cell_username.innerHTML = "Average";
-    cell_picks.innerHTML = "This is the average number of shares";
-    cell_delta.innerHTML = 0;
+    //cell_picks.innerHTML = "Avg. Shares";
+    cell_picks.innerHTML = numberWithCommas(meanRisked);
+    //cell_picks.innerHTML = 0.0;
+    cell_delta.innerHTML = 0.0;
     cell_delta.style.backgroundColor = "yellow";
+    cell_picks.style.backgroundColor = "yellow";
+    cell_delta.style.textAlign = "Right";
+    cell_picks.style.textAlign = "Right";
+    cell_shares.style.textAlign = "Right";
 }
 
-function getAverageShares() {
+function numberWithCommas(x) {
+    // Reformat number with commas
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+function getAverageShares(field) {
+    //Given a field number, get the average for that column
     var total = [];
     for (var i = 0, row; row = table.rows[i]; i++) {
-        var cell = row.cells[2]
+        var cell = row.cells[field]  // Specify field by var instead of hardcoded field
         var value = getIntValue(cell.innerHTML);
         if (!(isNaN(value))) {
             total.push(value);
@@ -74,8 +119,9 @@ function getAverageShares() {
     return mean;
 }
 
-meanShares = getAverageShares()
-addAverageRow(meanShares)
+meanShares = getAverageShares(2)  // Average shares total
+meanRisked = getAverageShares(3) // Average shares risked
+addAverageRow(meanShares, meanRisked)
 // Re-sort the table with Average included
 var script = document.createElement('script');
 script.innerHTML = "sortTable('bstandings', 2, true, true)";
